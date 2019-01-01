c500aa43bc2b78343fe264856c56632aeef60dd0
meytalOptimove <37654453+meytalOptimove@users.noreply.github.com>
2019-01-01 11:52:12 +0200

    Update and rename Trigger Pop-up upon Website Visit to Trigger Pop-up upon Website Visit.js

---
+++ eComm-Use-Cases-Code-Snippets/Trigger Pop-up upon Website Visit.js
@@ -0,0 +1,23 @@
+/**
+ * ***********NOTES**************
+ * Triggered: This will be triggered on every page visit
+ * There are various ways to implement this, such as via Tag Manager or directly into the website <head>
+    * For both options, make sure to add the <script> tag
+ */
+
+ /**
+ * Option 1: Tag Manager (e.g Google Tag Manager)
+    * GTM Tag/Event Name: page_loaded
+    * GTM Trigger Type: Custom Event
+    * GTM Trigger: Custom Event
+    * Built-In Variables  required: Event
+    * User-Defined Variables required: page_url, page_title, page_category
+ */
+self.optimoveSDK.API.reportEvent({{Event}}, {"page_url": {{page_url}}, "page_title": {{page_title}}, "page_category": {{page_category}}});
+
+
+ /**
+ * Option 2: Directly into the website <head>
+    * Event Name: page_loaded
+ */
+optimoveSDK.API.reportEvent('page_loaded', {"page_url": ""+document.location.href+"", "page_title": ""+document.title+"", "page_category": "depends on your wesite structure"});
