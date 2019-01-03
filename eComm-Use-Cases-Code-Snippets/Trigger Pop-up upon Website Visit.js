aff51c17662fa90a2dcd97dd93d61f9091a85634
meytalOptimove <37654453+meytalOptimove@users.noreply.github.com>
2019-01-03 11:27:14 +0200

    Update Trigger Pop-up upon Website Visit.js

--- eComm-Use-Cases-Code-Snippets/Trigger Pop-up upon Website Visit.js
+++ eComm-Use-Cases-Code-Snippets/Trigger Pop-up upon Website Visit.js
@@ -1,20 +1,23 @@
 /**
- * ***********NOTES**************
- * Triggered: This will be triggered on every page visit
+ * *********************************README**********************************
+ * Description: This will be triggered/fired on every page visit
  * There are various ways to implement this, such as via Tag Manager or directly into the website <head>
     * For both options, make sure to add the <script> tag
  */
 
+/************************************************************************ */
+
  /**
  * Option 1: Tag Manager (e.g Google Tag Manager)
     * GTM Tag/Event Name: page_loaded
-    * GTM Trigger Type: Custom Event
+    * GTM Trigger Type: Custom Event -> Custom HTML
     * GTM Trigger: Custom Event
-    * Built-In Variables  required: Event
-    * User-Defined Variables required: page_url, page_title, page_category
+    * GTM Built-In Variables  required: event_name
+    * GTM User-Defined Variables required: page_url, page_title, page_category
  */
-self.optimoveSDK.API.reportEvent({{Event}}, {"page_url": {{page_url}}, "page_title": {{page_title}}, "page_category": {{page_category}}});
+self.optimoveSDK.API.reportEvent({{event_name}}, {"page_url": {{page_url}}, "page_title": {{page_title}}, "page_category": {{page_category}}});
 
+/************************************************************************ */
 
  /**
  * Option 2: Directly into the website <head>
@@ -22,3 +25,4 @@ self.optimoveSDK.API.reportEvent({{Event}}, {"page_url": {{page_url}}, "page_tit
  */
 optimoveSDK.API.reportEvent('page_loaded', {"page_url": ""+document.location.href+"", "page_title": ""+document.title+"", "page_category": "depends on your wesite structure"});
 
+/************************************************************************ */
